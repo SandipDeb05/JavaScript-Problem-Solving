5c9934017ab02ee33b388dc34f57daa8bdef429f
SandipDeb <sandipdeb05@gmail.com>
2024-04-25 20:09:48 +0530

    solved prime number problem

--- maths/primeNumber.js
+++ maths/primeNumber.js
@@ -1,6 +1,15 @@
+function isPrimeNumber(num) {
+  if (num <= 1) return true;
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
 function printNPrimeNumbers(n) {
   for (let i = 1; i <= n; i++) {
-    // TODO
+    if (isPrimeNumber(i)) console.log(i);
   }
 }
-printNPrimeNumbers(10);
+
+printNPrimeNumbers(100);
